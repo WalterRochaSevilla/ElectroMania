c0ebed821d9fcc43f778f1a14ac10d87337d2981
Walter Rocha <walterrochasevilla@gmail.com>
2025-08-11 15:45:54 -0400

    correccion problema con FrontEnd

---
+++ electromania-frontend/src/app/pages/admin/productos-admin/productos-admin.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-productos-admin',
+  imports: [],
+  templateUrl: './productos-admin.component.html',
+  styleUrl: './productos-admin.component.css'
+})
+export class ProductosAdminComponent {
+
+}
