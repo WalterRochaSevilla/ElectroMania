2ca420aae4bd55c941fc1d32e2db97a8d6f81a4d
Matheo Quiroga <matheo.quiroga@ucb.edu.bo>
2025-08-12 11:55:24 -0400

    fix: Table name on User entity

--- Back_Electromanía/src/main/java/com/W/M/Back_Electromania/user/User.java
+++ Back_Electromanía/src/main/java/com/W/M/Back_Electromania/user/User.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Basic;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,6 +13,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
+@Table(name = "usuarios")
 public class User {
     @Id
     @GeneratedValue(strategy = jakarta.persistence.GenerationType.AUTO)
