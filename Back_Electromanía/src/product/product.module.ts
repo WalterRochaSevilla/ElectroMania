566445aa26cdbe9ebaca593c83f35e43c0bf61ae
Matheo Quiroga <matheo.quiroga@ucb.edu.bo>
2025-12-22 20:50:27 -0400

    feat: Login implementado con guards tanto de token como de roles

--- Back_Electromanía/src/product/product.module.ts
+++ Back_Electromanía/src/product/product.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { ProductService } from './service/product.service';
 import { ProductController } from './controller/product.controller';
 import { PrismaService } from 'src/prisma/service/prisma.service';
+import { AuthGuard } from 'src/auth/guards/auth.guard';
+import { RolesGuard } from 'src/auth/guards/roles.guard';
 
 @Module({
   imports: [
   ],
   controllers: [ProductController],
-  providers: [ProductService,PrismaService],
+  providers: [ProductService,PrismaService,AuthGuard,RolesGuard],
   exports: [ProductService],
 })
 export class ProductModule {}
