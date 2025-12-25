2747627aa144823c33d1a7008fd9e123bdd3d203
Nataly <nramirezmachicado@gmail.com>
2025-12-25 17:23:41 -0400

    Implementacion basica HTML,CSS,TS de la pagina login

--- electromania-frontend/src/app/app.component.ts
+++ electromania-frontend/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from "./pages/login/login.component";
 
 @Component({
   selector: 'app-root',
-  imports: [HomeComponent],
+  imports: [ LoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- electromania-frontend/src/app/pages/login/login.component.ts
+++ electromania-frontend/src/app/pages/login/login.component.ts
@@ -1,11 +1,47 @@
 import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+   /* =========================
+     ESTADOS GENERALES
+  ========================= */
+  modoOscuro: boolean = true;
 
+    // En tu componente
+  mostrarFormLogin: boolean = false;
+  email: string = '';
+  contrasena: string = '';
+  mostrarContrasena: boolean = false;
+
+  /* =========================
+     HEADER
+  ========================= */
+  cambiarModo() {
+    this.modoOscuro = !this.modoOscuro;
+  }
+  ingresar() {
+    console.log('Botón ingresar presionado');
+  }
+  
+  mostrarLogin() {
+    this.mostrarFormLogin = true;
+  }
+
+  alternarMostrarContrasena() {
+    this.mostrarContrasena = !this.mostrarContrasena;
+  }
+
+  iniciarSesion() {
+    console.log('Iniciando sesión con:', {
+      email: this.email,
+      contrasena: this.contrasena
+    });
+    // Aquí iría la lógica real de autenticación
+  }
 }
