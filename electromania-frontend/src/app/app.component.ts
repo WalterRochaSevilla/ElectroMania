f6e8d8e18552d5ef739fefefbc9705a4d00c8c78
Nataly <nramirezmachicado@gmail.com>
2025-12-28 22:44:17 -0400

    Implementacion de HTML,CSS,TS a la pagina productos-admin

--- electromania-frontend/src/app/app.component.ts
+++ electromania-frontend/src/app/app.component.ts
@@ -5,11 +5,12 @@ import { LoginComponent } from "./pages/login/login.component";
 import { RegistroComponent } from "./pages/registro/registro.component";
 import { ProductosComponent } from "./pages/productos/productos.component";
 import { DashboardComponent } from "./pages/admin/dashboard/dashboard.component";
+import { ProductosAdminComponent } from "./pages/admin/productos-admin/productos-admin.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [DashboardComponent],
+  imports: [ProductosAdminComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
