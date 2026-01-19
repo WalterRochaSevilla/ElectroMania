d7929a972d7b191739f763da2b119f4aee3626aa
Matheo Leonardo Quiroga Sanchez <matheo.quiroga@ucb.edu.bo>
2026-01-18 20:32:05 -0400

    feat: Se Obtiemen el resto de datos del usuario mediante su token de autenticacion generado por jwt

--- Back_Electromanía/src/user/user.module.ts
+++ Back_Electromanía/src/user/user.module.ts
@@ -1,13 +1,15 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { UserService } from './service/user.service';
 import { UserController } from './controller/user.controller';
 import { CommonModule } from '../common/common.module';
 import { PrismaService } from '../prisma/service/prisma.service';
 import { UserMapper } from './mapper/User.mapper';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
     imports: [
-        CommonModule
+        CommonModule,
+        forwardRef(() => AuthModule)
     ],
     controllers: [UserController],
     providers: [UserService,PrismaService,UserMapper],
